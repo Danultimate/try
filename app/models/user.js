afb387a69a986933e1fd932bb8e2915fb97f2b2e
sorelyss <sorboy11@gmail.com>
2018-09-06 15:21:16 -0500

    sign up working

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,9 @@ import attr from 'ember-data/attr';
 export default DS.Model.extend({
    first_name: attr('string'),
    last_name: attr('string'),
-   cellphone: attr('string'),
-   picture: attr('string')
-
+   cellphone: attr('number'),
+   identification: attr('number'),
+   picture: attr('string'),
+   password: attr('string'),
+   email: attr('string')
 });
