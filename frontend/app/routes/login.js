7d5766299aebde2c903f5e042965a4d6c6ef1005
sorelyss <sorboy11@gmail.com>
2018-09-07 08:40:02 -0500

    pretty frontend part 1

--- frontend/app/routes/login.js
+++ frontend/app/routes/login.js
@@ -3,4 +3,5 @@ import Route from '@ember/routing/route';
 // import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend({
+
 });
--- frontend/app/routes/task.js
+++ frontend/app/routes/task.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(params){
-        return this.store.peekRecord('task', params.task_id);
+        return this.store.findRecord('task', params.task_id);
     }
 });
