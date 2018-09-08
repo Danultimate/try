e4fd91c3246cab72ca0760aca4907c909360ec71
sorelyss <sorboy11@gmail.com>
2018-09-08 15:58:02 -0500

    Heroku client files

--- config/environment.js
+++ config/environment.js
@@ -20,7 +20,9 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    }, 
+
+    host: ''
   };
 
   if (environment === 'development') {
@@ -29,13 +31,11 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.host = 'https://localhost:5000';
   }
 
   if (environment === 'test') {
     // Testem prefers this...
     ENV.locationType = 'none';
-    ENV.host = 'https://localhost:5000';
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
