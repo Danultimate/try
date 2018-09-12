1dbfd9d966252a5e39d7e9454681bc6b749e5b4a
sorelyss <sorboy11@gmail.com>
2018-09-12 01:58:02 -0500

    Auth frontend test

--- frontend/app/authenticators/authenticator.js
+++ frontend/app/authenticators/authenticator.js
@@ -1,9 +1,10 @@
 import Oauth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
+import ENV from 'frontend/config/environment';
 
 export default Oauth2PasswordGrant.extend({
 
-    serverTokenEndpoint: 'api/login',
-    serverTokenRevocationEndpoint: 'api/login',
+    serverTokenEndpoint: ENV.host+'api/login',
+    serverTokenRevocationEndpoint: ENV.host+'api/login',
     // serverTokenEndpoint: 'https://localhost:5000/api/login',
     // serverTokenRevocationEndpoint: 'https://localhost:5000/api/logout'
 
