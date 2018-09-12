8064fe0a9b4c9cac3791248754f77bbcac33d701
sorelyss <sorboy11@gmail.com>
2018-09-12 02:04:17 -0500

    Auth frontend test

--- app/authenticators/authenticator.js
+++ app/authenticators/authenticator.js
@@ -3,8 +3,8 @@ import ENV from 'frontend/config/environment';
 
 export default Oauth2PasswordGrant.extend({
 
-    serverTokenEndpoint: ENV.host+'api/login',
-    serverTokenRevocationEndpoint: ENV.host+'api/login',
+    serverTokenEndpoint: ENV.host+'/api/login',
+    serverTokenRevocationEndpoint: ENV.host+'/api/login',
     // serverTokenEndpoint: 'https://localhost:5000/api/login',
     // serverTokenRevocationEndpoint: 'https://localhost:5000/api/logout'
 
