9db1f82981d658f80fb8c8760f91e1fb486a413c
sorelyss <sorboy11@gmail.com>
2018-09-13 16:36:00 -0500

    Order total changed

--- app/models/order.js
+++ app/models/order.js
@@ -5,19 +5,9 @@ import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
     order_number: attr('number'),
-    date: attr('date'),
     client: belongsTo('client'),
-    products: hasMany('product'),
     seller: belongsTo('seller'),
     status: attr('number'),
     date: attr('date'),
-    
-    // get prices of all related items (automatically updated when added/removed)
-    purchasePrices:  mapBy('products', 'price'),
-    // calculate sum of items
-    purchaseSum: sum('purchasePrices'),
-
-    // purchasePoints:  mapBy('product', 'point'),
-    // purchasePointsSum: sum('purchasePoints')
-
+    total: attr('number')
 });
