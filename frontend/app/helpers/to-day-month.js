5f8315d649f529359680b587264162ead0f9bbc2
Marlonny <warlony@gmail.com>
2018-09-19 14:33:11 -0500

    Change helper for user ember-moment library.

--- frontend/app/helpers/to-day-month.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function toDayMonth(params/*, hash*/) {
-  return params[0].getDay()+'/'+params[0].getMonth();
-}
-
-export default helper(toDayMonth);
