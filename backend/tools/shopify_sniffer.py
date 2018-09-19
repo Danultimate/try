1abd3c0c0904b726f6be97ded5c8784f323f843a
Marlonny <warlony@gmail.com>
2018-09-19 08:30:44 -0500

    Adding missing file

--- backend/tools/shopify_sniffer.py
+++ backend/tools/shopify_sniffer.py
@@ -29,6 +29,48 @@ def connect_to_shopify():
                                                                      password=SHOPIFY_PASSWORD,
                                                                      shopurl=SHOP_URL)
     shopify.ShopifyResource.set_site(shop_url)
+    print('Conected')
+
+def order_exists(order_number):
+    orders = db.session.query(Order).filter_by(order_number=order_number).all()
+    if len(orders) > 1: print('There are {0} duplicated orders for order_number: {1}'.format(len(orders), order_number))
+    print('Order alredy exists...' if len(orders) else 'Order not exists...')
+    return True if len(orders) else False
+
+
+def create_user(order):
+    u = None
+    try:
+        u = User(
+            first_name=order['customer']['first_name'],
+            last_name=order['customer']['last_name'],
+            cellphone=norm_cellphone(
+                order['customer']['default_address']['phone'])
+        )
+        db.session.add(u)
+        db.session.commit()
+        print('New Users was created!')
+    except Exception as e:
+        db.session.rollback()
+        print('Is not possible to save a new user into database!')
+        print(e)
+    return u
+
+
+def create_client(user_id, seller_id):
+    c = None
+    try:
+        c = Client(
+            old_consumer=False, user_id=user_id, profile_id=1, seller_id=seller_id
+        )
+        db.session.add(c)
+        db.session.commit()
+        print('New Client was created!')
+    except Exception as e:
+        db.session.rollback()
+        print('Is not possible to save a new client into database!')
+        print(e)
+    return c
 
 
 def norm_cellphone(phone):
@@ -37,7 +79,7 @@ def norm_cellphone(phone):
 
 def get_max_id_from_file():
     print('-------------------------------------------------------')
-    f = open("./backend/tools/max_id.txt", "r")
+    f = open("max_id.txt", "r")
     max_id = f.read()[:-1]
     f.close()
     # Delete file, afeter the orders iteration will be write again
@@ -46,7 +88,7 @@ def get_max_id_from_file():
 
 
 def create_new_max_id_file(max_id):
-    f = open("./backend/tools/max_id.txt", "w")
+    f = open("max_id.txt", "w")
     f.write(str(max_id) + '\n')
     print('Saving the max_id value in file: max_id = {0}'.format(max_id))
     f.close()
@@ -72,10 +114,10 @@ def get_orders_from_shopify():
 
 def save_trouble_order(order_id, message):
     # Read the file:
-    f = open("./backend/tools/trouble_orders.txt", "r")
+    f = open("trouble_orders.txt", "r")
     lines = f.readlines()
     f.close()
-    f = open("./backend/tools/trouble_orders.txt", "w")
+    f = open("trouble_orders.txt", "w")
     f.write(str(order_id) + "," + message + "\n")
     for line in lines:
         f.write(line)
@@ -83,17 +125,23 @@ def save_trouble_order(order_id, message):
     f.close()
 
 
-def update_order_table(orders):
+def create_orders(orders):
     # TODO: Control exceptions and keep the file with max_id
     if len(orders):
         for order in orders:
             order = order.to_dict()
+
+            if order_exists(str(order['order_number'])):
+                create_new_max_id_file(order['id'])
+                continue
+
             # Check if phone is empty
             if order['customer']['default_address']['phone'] is None:
                 save_trouble_order(
                     order['id'], 'Not phone found into customer-> default_address -> phone')
                 create_new_max_id_file(order['id'])
                 continue
+
             # Check if phone contains only numbers
             regex = re.compile("^[0-9]+$")
             phone = norm_cellphone(
@@ -106,77 +154,55 @@ def update_order_table(orders):
                     order['id'], 'The phone number into order is not a valid number')
                 create_new_max_id_file(order['id'])
                 continue
-            user_client_id = db.session.query(User.id).filter_by(
-                cellphone=norm_cellphone(order['customer']['default_address']['phone'])).first()
 
-            # Check if user_client_id query returns None, see above ^^
-            if user_client_id is not None:
-                user_client_id = user_client_id[0]
-            # Seller 1 it's the anonymous seller, is used when clients creates order without a seller disccount code
+            user_client_id = None
+            u = db.session.query(User).filter_by(
+                cellphone=norm_cellphone(order['customer']['default_address']['phone'])).first()
+            if u is not None:
+                user_client_id = u.id
+
+            # Seller 1 it's descubre's seller, is used for orders without a seller disccount code
+            seller_id = 5
             if len(order['discount_codes']):
-                seller_id = db.session.query(Seller.id).filter_by(code=order['discount_codes'][0]['code']).first()[
-                    0] if db.session.query(Seller.id).filter_by(code=order['discount_codes'][0]['code']).first() else None
-            else:
-                seller_id = 1
+                seller_id = db.session.query(Seller.id).filter_by(code=order['discount_codes'][0]['code']).first() \
+                    if db.session.query(Seller.id).filter_by(code=order['discount_codes'][0]['code']).first() else None
 
             print('Processing order {0}...'.format(order['id']))
 
             if user_client_id is None:
-                # Let's create a new one:
-                u = User(
-                    first_name=order['customer']['first_name'],
-                    last_name=order['customer']['last_name'],
-                    cellphone=norm_cellphone(
-                        order['customer']['default_address']['phone'])
-                )
-                db.session.add(u)
-                try:
-                    db.session.commit()
-                except Exception as e:
-                    db.session.rollback()
-                    print('Is not possible to save a new user into database!')
-                    print(e)
-
-                c = Client(
-                    old_consumer=False, user_id=u.id, profile_id=1, seller_id=seller_id
-                )
-                db.session.add(c)
-
-                try:
-                    db.session.commit()
-                except Exception as e:
-                    db.session.rollback()
-                    print('Is not possible to save a new client into database!')
-                    print(e)
-
-                client_id = c.id
+                u = create_user(order)
+                c = create_client(u.id, seller_id)
             else:
-                client_id = db.session.query(Client.id).filter_by(user_id=user_client_id).first()[
-                    0] if db.session.query(Client.id).filter_by(user_id=user_client_id).first() else None
+                c = db.session.query(Client).filter_by(user_id=user_client_id).first() \
+                        if db.session.query(Client).filter_by(user_id=user_client_id).first() else None
+                if c is None:
+                    c = create_client(u.id, seller_id)
+            client_id = c.id
 
-            order_ = Order(
-                seller_id=seller_id,
-                client_id=client_id,
-                date=order['updated_at'],
-                order_number=order['order_number'],
-                total=order['total_price'],
-                tax=order['total_tax'],
-                shipping=order['shipping_lines'][0]['price']
-            )
-            db.session.add(order_)
-
+            # Create the order:
             try:
+                order_ = Order(
+                    seller_id=seller_id,
+                    client_id=client_id,
+                    date=order['updated_at'],
+                    order_number=order['order_number'],
+                    total=order['total_price'],
+                    tax=order['total_tax'],
+                    shipping=order['shipping_lines'][0]['price']
+                )
+                db.session.add(order_)
                 db.session.commit()
+                print('-------------------------- Begin Order --------------------------')
+                print('Order Created. ID: {0}, NAME: {1}, TOTAL: {2}, EMAIL: {3}, PHONE: {4}'
+                      .format(order['id'], order['name'], order['total_price'], order['customer']['email'], order['customer']['default_address']['phone']))
+                print('-------------------------- End Order --------------------------')
             except Exception as e:
                 db.session.rollback()
-                print('Is not possible to save a new user into database!')
+                print('Is not possible to save the Orders into database')
+                save_trouble_order(order['id'], str(e))
                 print(e)
 
             create_new_max_id_file(order['id'])
-            print('-------------------------- Begin Order --------------------------')
-            print('Saving order. ID: {0}, NAME: {1}, TOTAL: {2}, EMAIL: {3}, PHONE: {4}'
-                  .format(order['id'], order['name'], order['total_price'], order['customer']['email'], order['customer']['default_address']['phone']))
-            print('-------------------------- End Order --------------------------')
     else:
         print('Nothing to do...')
 
@@ -184,7 +210,9 @@ def update_order_table(orders):
 def main():
     connect_to_shopify()
     while True:
-        time.sleep(SECONDS_TO_FETCH)
         orders = get_orders_from_shopify()
         print('Orders List: {0}'.format(orders))
-        update_order_table(orders)
+        create_orders(orders)
+        time.sleep(SECONDS_TO_FETCH)
+
+main()
