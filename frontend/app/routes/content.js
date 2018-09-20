66f85255115fb6d9610f07637d8ca16c148561d8
Marlonny <warlony@gmail.com>
2018-09-20 17:22:40 -0500

    Fix usage and import for Ember.RSVP.hash

--- frontend/app/routes/content.js
+++ frontend/app/routes/content.js
@@ -1,10 +1,10 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-
+import { hash } from 'rsvp';
 
 export default Route.extend(AuthenticatedRouteMixin, {
     model(params){
-        return Ember.RSVP.hash({
+        return hash({
             content: this.store.findRecord('content', params.content_id),
             clients: this.store.findAll('client')
           });
