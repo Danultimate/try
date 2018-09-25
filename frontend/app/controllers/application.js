444e832f07f2172576e7b0f0223e0a15314a99bc
sorelyss <sorboy11@gmail.com>
2018-09-25 16:48:14 -0500

    A little bit better performance

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -1,7 +1,5 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { observer } from '@ember/object';
-import { next } from '@ember/runloop';
 
 export default Controller.extend({
 
