612ba5f361eec53de315d61bb69ddb5d06f26bfa
sorelyss <sorboy11@gmail.com>
2018-09-26 10:52:04 -0500

    Flask admin users name change

--- app/models/task.js
+++ app/models/task.js
@@ -5,39 +5,39 @@ import { belongsTo, hasMany } from 'ember-data/relationships';
 import { computed } from '@ember/object';
 
 export default Model.extend({
-    task_description: attr('string'),
-    planned_date: attr('date'),
-    num_of_clients: attr('number'),
-    done: attr('boolean', { defaultValue: false }),
-    excuted_date: attr('date'),
-    users: hasMany('user'),
-    // social_media: hasMany('')
-    client_suggestions: hasMany('client-suggestions'),
-    seller: belongsTo('seller'),
-    content: belongsTo('content'),
+  task_description: attr('string'),
+  planned_date: attr('date'),
+  num_of_clients: attr('number'),
+  done: attr('boolean', { defaultValue: false }),
+  excuted_date: attr('date'),
+  users: hasMany('user'),
+  // social_media: hasMany('')
+  client_suggestions: hasMany('client-suggestions'),
+  seller: belongsTo('seller'),
+  content: belongsTo('content'),
 
-    filteredClientSuggestions: computed('users', function () {
-      //FIXME: Delete this when we can filter the full list into the front, (i.e. with a responsive
-      //table or 'show more', show less options)
-      let clientSuggestionsLimit = 5;
-      let clientSuggestions = this.get('users');
-      console.log(`This is clientSuggestions ${clientSuggestions}`);
-      let filteredResponse = [];
+  filteredClientSuggestions: computed('users', function () {
+    //FIXME: Delete this when we can filter the full list into the front, (i.e. with a responsive
+    //table or 'show more', show less options)
+    let clientSuggestionsLimit = 5;
+    let clientSuggestions = this.get('users');
+    console.log(`This is clientSuggestions ${clientSuggestions}`);
+    let filteredResponse = [];
 
-      if (clientSuggestionsLimit < clientSuggestions.length ) {
-        for (let i=0; i<clientSuggestionsLimit; i++) {
-          filteredResponse.push(clientSuggestions[i]);
-          console.log(clientSuggestions[i]);
-        }
-      } else {
-        for (let i=0; i<=clientSuggestions.length; i++) {
-          filteredResponse.push(clientSuggestions[i]);
-          console.log(`clientSuggestions[${i}] = ${clientSuggestions[i]}`);
-          console.log(`clientSuggestions[] length = ${clientSuggestions.length}`);
-        }
+    if (clientSuggestionsLimit < clientSuggestions.length) {
+      for (let i = 0; i < clientSuggestionsLimit; i++) {
+        filteredResponse.push(clientSuggestions[i]);
+        console.log(clientSuggestions[i]);
       }
-      return filteredResponse
-    })
+    } else {
+      for (let i = 0; i <= clientSuggestions.length; i++) {
+        filteredResponse.push(clientSuggestions[i]);
+        console.log(`clientSuggestions[${i}] = ${clientSuggestions[i]}`);
+        console.log(`clientSuggestions[] length = ${clientSuggestions.length}`);
+      }
+    }
+    return filteredResponse
+  })
 
 
 
