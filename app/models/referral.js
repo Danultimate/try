09b1cb6117ab98a9b8ed07bb477ec404fc68be5e
sorelyss <sorboy11@gmail.com>
2018-09-27 18:36:11 -0500

    Referral system backend

--- app/models/referral.js
+++ app/models/referral.js
@@ -1,8 +1,11 @@
 import DS from 'ember-data';
 import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
 
 export default DS.Model.extend({
-    referred_by: attr('string'),
-    referred_by: attr('string'),
-    paid: attr('boolean')
+    referred_by: belongsTo('seller'),
+    referred: attr('string'),
+    paid: attr('boolean'),
+    
+    //seller: belongsTo('seller'),
 });
--- app/models/seller.js
+++ app/models/seller.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 import { hasMany, belongsTo } from 'ember-data/relationships';
 import { mapBy, sum, filterBy } from '@ember/object/computed';
 import attr from 'ember-data/attr';
+import { schedule } from '@ember/runloop';
 
 export default DS.Model.extend({
     commission: attr('number'),
@@ -11,7 +12,7 @@ export default DS.Model.extend({
     clients: hasMany('client'),
     tasks: hasMany('task'),
     notifications: hasMany('notification'),
-
+    
     total_per_order:  mapBy('orders', 'total'),
     tax_per_order:  mapBy('orders', 'tax'),
     shipping_per_order:  mapBy('orders', 'shipping'),
@@ -20,13 +21,30 @@ export default DS.Model.extend({
     total_shipping: sum('shipping_per_order'),
     total_tax: sum('tax_per_order'),
 
-    // total_points_per_order:  mapBy('orders', 'purchasePointsSum'),
-    // total_points: sum('total_points_per_order'),
-
     tasksCompleted: filterBy('tasks', 'done', true),
     tasksUncompleted: filterBy('tasks', 'done', false),
 
     ordersActive: filterBy('orders', 'status', 'ordered'),
     ordersDitribution: filterBy('orders', 'status', 'distribution'),
     
+    /*
+    init() {
+        this._super();
+        schedule("afterRender",this,function() {
+          this.send("get_referrals");
+        });
+      },
+
+    referrals: null,
+    non_paid_referrals: filterBy('referrals', 'paid', false), */
+
+    actions: {
+        get_referrals() {
+            this.store.findAll('referral')
+            .then(function(records) {
+                console.log(records);
+                //this.set('referrals', records)
+            });
+        }
+    }
 });
