6fc83de89e452e59a694652ae7f9e942ae3319b7
sorelyss <sorboy11@gmail.com>
2018-10-02 15:32:52 -0500

    Mixpanel events and landing better looking

--- app/controllers/orders.js
+++ app/controllers/orders.js
@@ -1,16 +1,14 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
 import { schedule } from '@ember/runloop';
 
 export default Controller.extend({
-    
-    session: service('session'),
 
     init() {
         this._super();
         schedule("afterRender",this,function() {
-            localStorage.clear();
+            window.mixpanel.track('watch orders');
         });
       },
-    
+
+
 });
