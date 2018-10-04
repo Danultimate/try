e1416e7baa97a42a385a2f26044719c5edbc11b9
sorelyss <sorboy11@gmail.com>
2018-10-04 13:12:46 -0500

    Content visual no flex

--- app/app.js
+++ app/app.js
@@ -13,16 +13,12 @@ loadInitializers(App, config.modulePrefix);
 
 Ember.onerror = function (error) {
   console.log('Ember.onerror handler', error.message);
-  localStorage.clear();
-  window.location = '/';
+  //localStorage.clear();
+  //window.location = '/';
 };
 
 Ember.RSVP.on('error', function (error) {
   console.log('Ember.RSVP error handler', error);
 });
-
-Ember.Logger.error = function (message, cause, stack) {
-  console.log('Ember.Logger.error handler', message, cause, stack);
-};
 
 export default App;
