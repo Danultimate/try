fb9fe487e916e9788f577278a747d459850a648b
SalazarPardo <jc@salazarpardo.com>
2018-10-17 22:18:25 -0500

    Added react native starter kit

---
+++ react-native/src/constants/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyBRt6tMqb80yenhPgDtOPyseZPSru41BuA",
+  authDomain: "elenas-react.firebaseapp.com",
+  databaseURL: "https://elenas-react.firebaseio.com",
+  projectId: "elenas-react",
+  storageBucket: "elenas-react.appspot.com",
+  messagingSenderId: "367365028708"
+};
