a377d4bd0561974d6fc2cf306bb27e4bd1ae79ac
SalazarPardo <jc@salazarpardo.com>
2018-10-31 16:43:22 -0500

    added success card to dashboard again

--- react-native/src/native/components/Dashboard.js
+++ react-native/src/native/components/Dashboard.js
@@ -128,6 +128,66 @@ class Dashboard extends React.Component {
                 paddingBottom: 0
               }}
             >
+              <Image source={require("../assets/images/msg-success.png")} />
+            </CardItem>
+            <CardItem style={styles.cardBody}>
+              {/* aqui colocar el condicional */}
+              <Body>
+                <H3
+                  style={[styles.header, styles.successMsg, styles.textCenter]}
+                >
+                  ¡Eres una vendedora super poderosa!
+                </H3>
+                <Text style={[styles.meta, styles.date]}>Hace 26 minutos</Text>
+                <Spacer size={8} />
+                <Text style={styles.description}>
+                  Tus ventas de la semana pasada te colocan dentro de nuestras
+                  mejores 50 vendedoras. ¡Felicitaciones!
+                </Text>
+                <Spacer size={16} />
+              </Body>
+              {/* aqui terminar el condicional */}
+            </CardItem>
+            <CardItem style={styles.cardFooter} footer bordered>
+              <Left>
+                <Button
+                  style={styles.cardButton}
+                  block
+                  transparent
+                  info
+                  small
+                  iconLeft
+                  onPress={() => onPress(item)}
+                >
+                  <Icon type="SimpleLineIcons" name="heart" />
+                  <Text style={styles.cardButtonText}>Me encanta</Text>
+                </Button>
+              </Left>
+              <Right>
+                <Button
+                  style={styles.cardButton}
+                  block
+                  transparent
+                  info
+                  small
+                  iconLeft
+                  onPress={() => onPress(item)}
+                >
+                  <Icon type="SimpleLineIcons" name="share-alt" />
+                  <Text style={styles.cardButtonText}>Compartir</Text>
+                </Button>
+              </Right>
+            </CardItem>
+          </Card>
+          <Card style={styles.card}>
+            <CardItem
+              header
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                paddingBottom: 0
+              }}
+            >
               <Image source={require("../assets/images/msg-warning.png")} />
             </CardItem>
             <CardItem style={styles.cardBody}>
