d17a6976c13ce0b289f00e5048ac93e93b4ac3c4
sorelyss <sorboy11@gmail.com>
2018-12-13 16:09:10 -0500

    wootric implementation

--- react-native/android/app/src/main/java/host/exp/exponent/MainApplication.java
+++ react-native/android/app/src/main/java/host/exp/exponent/MainApplication.java
@@ -10,6 +10,7 @@ import expolib_v1.okhttp3.OkHttpClient;
 
 // Needed for `react-native link`
 // import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNWootricPackage;
 import io.sentry.RNSentryPackage;
 
 public class MainApplication extends ExpoApplication {
@@ -28,6 +29,7 @@ public class MainApplication extends ExpoApplication {
 
         // Needed for `react-native link`
         // new MainReactPackage(),
+            new RNWootricPackage(),
             new RNSentryPackage()
     );
   }
